bbb1e9f16d9517d33594cf3c7a1966b5960d66fd
udowo <18100586+udonko777@users.noreply.github.com>
2025-06-06 22:05:56 +0900

    feat!: index.tsで公開する関数を狭めた

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
-export * as f64Mat from "./matrix";
+export { isf64Mat, fromColumnMajor, fromRowMajor, getClone, getIdentity, init, valueAt, add, subtract, multiply, equals, sameSize, inverse, determinant, toString } from "./matrix";
+export type { f64Mat } from "@/matrix"
