73289a00270517d3f6cbe08b308aa3f471970a23
udowo <18100586+udonko777@users.noreply.github.com>
2025-06-26 21:31:17 +0900

    feat: ライブラリの出力方式にnamespaceを使用するのを辞めた

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,42 @@
-/* eslint-disable */
-import * as f64 from "./f64Mat";
+import * as f64M from "./f64Mat";
 
-export namespace f64Mat {
-  export const isf64Mat = f64.isF64Mat;
-  export const fromColumnMajor = f64.fromColumnMajor;
-  export const fromRowMajor = f64.fromRowMajor;
-  export const getClone = f64.getClone;
-  export const getIdentity = f64.getIdentity;
-  export const init = f64.init;
-  export const valueAt = f64.valueAt;
-  export const add = f64.add;
-  export const subtract = f64.subtract;
-  export const multiply = f64.multiply;
-  export const equals = f64.equals;
-  export const sameSize = f64.sameSize;
-  export const inverse = f64.inverse;
-  export const determinant = f64.determinant;
-  export const toString = f64.toString;
+export type { F64Mat } from "./f64Mat";
+export type { F32Mat } from "./f32Mat";
+
+export const f64Mat = {
+  isF64Mat: f64M.isF64Mat,
+  fromColumnMajor: f64M.fromColumnMajor,
+  fromRowMajor: f64M.fromRowMajor,
+  getClone: f64M.getClone,
+  getIdentity: f64M.getIdentity,
+  init: f64M.init,
+  valueAt: f64M.valueAt,
+  add: f64M.add,
+  subtract: f64M.subtract,
+  multiply: f64M.multiply,
+  equals: f64M.equals,
+  sameSize: f64M.sameSize,
+  inverse: f64M.inverse,
+  determinant: f64M.determinant,
+  toString: f64M.toString,
 }
 
 import * as f32 from "./f32Mat";
 
-export namespace f32Mat {
-  export const isf32Mat = f32.isF32Mat;
-  export const fromColumnMajor = f32.fromColumnMajor;
-  export const fromRowMajor = f32.fromRowMajor;
-  export const getClone = f32.getClone;
-  export const getIdentity = f32.getIdentity;
-  export const init = f32.init;
-  export const valueAt = f32.valueAt;
-  export const add = f32.add;
-  export const subtract = f32.subtract;
-  export const multiply = f32.multiply;
-  export const equals = f32.equals;
-  export const sameSize = f32.sameSize;
-  export const inverse = f32.inverse;
-  export const determinant = f32.determinant;
-  export const toString = f32.toString;
+export const f32Mat ={
+  isF32Mat: f32.isF32Mat,
+  fromColumnMajor: f32.fromColumnMajor,
+  fromRowMajor: f32.fromRowMajor,
+  getClone: f32.getClone,
+  getIdentity: f32.getIdentity,
+  init: f32.init,
+  valueAt: f32.valueAt,
+  add: f32.add,
+  subtract: f32.subtract,
+  multiply: f32.multiply,
+  equals: f32.equals,
+  sameSize: f32.sameSize,
+  inverse: f32.inverse,
+  determinant: f32.determinant,
+  toString: f32.toString,
 }
